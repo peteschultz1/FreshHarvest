c4e0f9ee8d99b9b80457517965bd6dcc836afffc
peteschultz1 <44955457+peteschultz1@users.noreply.github.com>
2023-03-29 14:24:41 -0500

    Create MainStyle.css

---
+++ MainStyle.css
@@ -0,0 +1,80 @@
+/* Navbar */
+.navbar {
+	position: relative;
+	background-color: #4CAF50;
+	height: 60px;
+	line-height: 60px;
+	padding: 0 20px;
+}
+
+.logo {
+	float: left;
+	font-size: 24px;
+	font-weight: bold;
+	color: white;
+	text-decoration: none;
+}
+
+.menu-icon {
+	float: right;
+	display: block;
+	color: white;
+	font-size: 28px;
+	line-height: 60px;
+	cursor: pointer;
+}
+
+.menu-icon:hover {
+	color: #ccc;
+}
+
+/* Dropdown menu */
+.menu {
+	display: none;
+	position: absolute;
+	top: 60px;
+	right: 0;
+	background-color: #f9f9f9;
+	min-width: 160px;
+	box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+	padding: 12px 16px;
+	z-index: 1;
+}
+
+.menu a {
+	color: #4CAF50;
+	padding: 12px 16px;
+	display: block;
+}
+
+.menu a:hover {
+	background-color: #f1f1f1;
+}
+
+/* Content */
+.content {
+	padding: 50px;
+	text-align: center;
+}
+
+.content h1 {
+	margin-bottom: 20px;
+}
+
+.content p {
+	margin-bottom: 30px;
+}
+
+.button {
+	background-color: #4CAF50;
+	color: white;
+	border: none;
+	padding: 10px 20px;
+	border-radius: 4px;
+	cursor: pointer;
+	font-size: 18px;
+}
+
+.button:hover {
+	background-color: #45a049;
+}
